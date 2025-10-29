639d85a87ee63a71ebd35c6c709b8ded3c16f550
Procurementanalys <virzaldi@gmail.com>
2025-10-29 13:32:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace '<YOUR_REPO_NAME>' with the name of your GitHub repository
-  // For example, if your repo URL is https://github.com/user/my-app, set base to '/my-app/'
-  base: 'OCTOBULL', 
+  // IMPORTANT: This path must match your GitHub repository name exactly.
+  // This tells Vite where the assets will be located on the server.
+  base: '/OCTOBULL/', 
 })
